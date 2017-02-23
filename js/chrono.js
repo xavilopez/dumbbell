5d2795ecb5eb68743d94edf8f4efdbe26816a005
xavilopez <xavi_lopez011@hotmail.com>
2017-02-23 11:07:28 -0300

    circleTimer added

--- js/chrono.js
+++ js/chrono.js
@@ -1,31 +1,36 @@
 $(function() {
   var counter;
+  var counterMs;
   var topCount = 30;
   var count = topCount;
   $('#timerFito').text(count);
+  var topCountMs = 100;
+  var countMs = topCountMs;
+  $('.timerseg').text(countMs);
   var isPaused = true;
 
 $('#chronobtn').on('click',()=>{
-  console.log($('#chronobtn').hasClass('glyphicon-repeat'));
   if ($('#chronobtn').hasClass('glyphicon-repeat')) {
     count = topCount;
     $('#timerFito').text(count);
     $('#chronobtn').removeClass('glyphicon-repeat');
     $('#chronobtn').addClass('glyphicon-play');
+
   }else if ($('#chronobtn').hasClass('glyphicon-pause')){
       $('#chronobtn').removeClass('glyphicon-pause');
       $('#chronobtn').addClass('glyphicon-play');
       clearInterval(counter);
+      clearInterval(counterMs);
 
   }else if ($('#chronobtn').hasClass('glyphicon-play')){
       $('#chronobtn').removeClass('glyphicon-play');
       $('#chronobtn').addClass('glyphicon-pause');
-      counter = setInterval(timerFito, 1000); //10 will  run it every 100th of a second
+      counter = setInterval(timerFito, 1000);
+      counterMs = setInterval(timerMS, 10); //10 will  run it every 100th of a second
   }
 })
 
-function timerFito()
-{
+function timerFito(){
     if (count <= 0)
     {
         $('#chronobtn').removeClass('glyphicon-pause');
@@ -36,6 +41,10 @@ function timerFito()
      }
      count--;
     $("#timerFito").text(count);
+}
+
+function timerMS(){
+  console.log("asd");
 }
 
 
